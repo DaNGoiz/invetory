eb34a9e6f9a0053d5918350218761bcf3a4c0712
Izzy <3332099165@qq.com>
2024-03-27 19:35:24 +0800

    Load item script

---
+++ Inventory/Assets/Scripts/Inventory/LoadItems.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadItems : MonoBehaviour
+{
+    // Item: name, description, icon, type, stackable, maxStack, value
+    
+}
