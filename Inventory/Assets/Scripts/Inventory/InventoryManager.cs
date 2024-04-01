eebb52e464bfb9bdffb027fd14c2fb3ea3217269
Izzy <3332099165@qq.com>
2024-04-01 18:20:14 +0800

    Numpad select

--- Inventory/Assets/Scripts/Inventory/InventoryManager.cs
+++ Inventory/Assets/Scripts/Inventory/InventoryManager.cs
@@ -7,6 +7,34 @@ public class InventoryManager : MonoBehaviour
     public InventorySlot[] inventorySlots;
     public GameObject inventoryItemPrefab;
 
+    int selectedSlotIndex = -1;
+
+    private void Start()
+    {
+        ChangeSelectedSlot(0);
+    }
+
+    private void Update()
+    {
+        if (Input.inputString != null)
+        {
+            bool isNumber = int.TryParse(Input.inputString, out int number);
+            if (isNumber)
+            {
+                if (number > 0 && number < 10) ChangeSelectedSlot(number - 1);
+                if (number == 0) ChangeSelectedSlot(9);
+            }
+        }
+    }
+
+    void ChangeSelectedSlot(int newValue)
+    {
+        if (selectedSlotIndex >= 0)
+            inventorySlots[selectedSlotIndex].Deselect();
+        inventorySlots[newValue].Select();
+        selectedSlotIndex = newValue;
+    }
+
     public bool AddItem(Item item)
     {
         // find stackable
